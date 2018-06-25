e8e3e3d248c17da05b5f0dcef0197f6b8d14fea6
LDQ-nanchang <929041681@qq.com>
2018-06-25 15:08:55 +0800

    优化ie兼容性问题

--- js/index.js
+++ js/index.js
@@ -2,8 +2,6 @@ $(document).ready(function () {
 	var brower_type = '';
 	var get_browser_type = function () {
 		var agent = navigator.userAgent.toLowerCase();
-		agent ="Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET4.0C; .NET4.0E; InfoPath.2; rv:11.0) like Gecko"
-
 		var regStr_ie = /msie [\d.]+;/gi;
 		var regStr_ff = /firefox\/[\d.]+/gi
 		var regStr_chrome = /chrome\/[\d.]+/gi;
